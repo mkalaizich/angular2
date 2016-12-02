65d15df9d128495ec2ea1650522e8c363f45c006
Martin Kalaizich <martin.kalaizich@globant.com>
2016-12-02 15:11:28 -0300

    Martin Kalaizich - Hero Editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
 
-@Component({
-    moduleId: module.id,
-    selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <nav>
-            <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-            <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-        </nav>
-        <router-outlet></router-outlet>
-    `,
-    styleUrls: ['app.component.css'],
-})
-
-export class AppComponent {
-
-    title = 'Tour of Heroes';
+export class Hero {
+  id: number;
+  name: string;
 }
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
